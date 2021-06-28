1ac815a43229817b6dd1004584e47d121a80d385
Toykyle <67622169+Toykyle@users.noreply.github.com>
2021-06-28 00:36:14 -0400

    Stuff

---
+++ www.ndreams.com/xijess/quiz.js
@@ -0,0 +1,138 @@
+﻿function checkanswer1(form)
+{
+  // ** START **
+
+  if (form.playerinput.value.toUpperCase() == 'MILAN' || form.playerinput.value.toUpperCase()== 'MILAN ' ||
+      form.playerinput.value.toUpperCase() == 'MILANO' || form.playerinput.value.toUpperCase() == 'MILANO ' ||
+      form.playerinput.value.toUpperCase() == 'MAILAND' || form.playerinput.value.toUpperCase() == 'MAILAND ' ||
+      form.playerinput.value.toUpperCase() == 'MILÁN' || form.playerinput.value.toUpperCase() == 'MILÁN ' ) {
+      location.href="page1_aith.html";
+    return false;
+  }
+  else
+  {
+    form.playerinput.focus();
+    location.href="wrong.html";
+    return false ;
+  }
+  return false ;
+}
+
+function checkanswer2(form)
+{
+  // ** START **
+  if (form.playerinput.value == '1854' || form.playerinput.value == '1854 ' ) {
+      location.href="page2_jmnw.html";
+    return false;
+  }
+  else
+  {
+    form.playerinput.focus();
+    location.href="wrong.html";
+    return false ;
+  }
+  return false ;
+}
+
+function checkanswer3(form)
+{
+  // ** START **
+  if (form.playerinput.value.toUpperCase() == 'ROME' || form.playerinput.value.toUpperCase() == 'ROME ' ||                   form.playerinput.value.toUpperCase() == 'ROMA' || form.playerinput.value.toUpperCase() == 'ROMA ' ||
+      form.playerinput.value.toUpperCase() == 'ROM' || form.playerinput.value.toUpperCase() == 'ROM ' ) {
+      location.href="page3_aazz.html";
+    return false;
+  }
+  else
+  {
+    form.playerinput.focus();
+    location.href="wrong.html";
+    return false ;
+  }
+  return false ;
+}
+
+function checkanswer4(form)
+{
+  // ** START **
+  if (form.playerinput.value.toUpperCase() == '259' || form.playerinput.value.toUpperCase() == '259 ' ||             form.playerinput.value.toUpperCase() == '259M' || form.playerinput.value.toUpperCase() == '259M ' ||
+      form.playerinput.value.toUpperCase() == '259 METRES' || form.playerinput.value.toUpperCase() == '259 METRES ' ) {
+      location.href="page4_oosk.html";
+    return false;
+  }
+  else
+  {
+    form.playerinput.focus();
+    location.href="wrong.html";
+    return false ;
+  }
+  return false ;
+}
+
+function checkanswer5(form)
+{
+  // ** START **
+  if (form.playerinput.value.toUpperCase() == 'KINKS' || form.playerinput.value.toUpperCase() == 'KINKS ' ||                   form.playerinput.value.toUpperCase() == 'THE KINKS' || form.playerinput.value.toUpperCase() == 'THE KINKS '  ) {
+      location.href="page5_lsdg.html";
+    return false;
+  }
+  else
+  {
+    form.playerinput.focus();
+    location.href="wrong.html";
+    return false ;
+  }
+  return false ;
+}
+
+function checkanswer6(form)
+{
+  // ** START **
+	if (form.playerinput.value.toUpperCase() == 'GAUDI' || form.playerinput.value.toUpperCase() == 'GAUDI ' ||             	form.playerinput.value.toUpperCase() == 'ANTONI GAUDI' || form.playerinput.value.toUpperCase() == 'ANTONI GAUDI ' ||
+      form.playerinput.value.toUpperCase() == 'ANTONI GAUDÍ' || form.playerinput.value.toUpperCase() == 'ANTONI GAUDÍ ' ||
+      form.playerinput.value.toUpperCase() == 'GAUDÍ' || form.playerinput.value.toUpperCase() == 'GAUDÍ ' ) {
+      location.href="page6_uiuz.html";
+    return false;
+  }
+  else
+  {
+    form.playerinput.focus();
+    location.href="wrong.html";
+    return false ;
+  }
+  return false ;
+}
+
+function checkanswer7(form)
+{
+  // ** START **
+  if (form.playerinput.value.toUpperCase() == 'TOLEDO' || form.playerinput.value.toUpperCase() == 'TOLEDO ' ||             form.playerinput.value.toUpperCase() == 'TOLEDE' || form.playerinput.value.toUpperCase() == 'TOLEDE ' ||
+      form.playerinput.value.toUpperCase() == 'TOLÈDE' || form.playerinput.value.toUpperCase() == 'TOLÈDE '  ) {
+      location.href="page7_ppzx.html";
+    return false;
+  }
+  else
+  {
+    form.playerinput.focus();
+    location.href="wrong.html";
+    return false ;
+  }
+  return false ;
+}
+
+function checkanswer8(form)
+{
+  // ** START **
+  if (form.playerinput.value.toUpperCase() == 'JFK' || form.playerinput.value.toUpperCase() == 'JFK ' ||             form.playerinput.value.toUpperCase() == 'JOHN F KENNEDY' || form.playerinput.value.toUpperCase() == 'JOHN F KENNEDY ' ||
+      form.playerinput.value.toUpperCase() == 'JOHN F. KENNEDY' || form.playerinput.value.toUpperCase() == 'JOHN F. KENNEDY ' ) {
+      location.href="page8_ejxl.html";
+    return false;
+  }
+  else
+  {
+    form.playerinput.focus();
+    location.href="wrong.html";
+    return false ;
+  }
+  return false ;
+}
+
